569d90ee7b94aa08a803275aab5258ed56c5ad3f
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-07 07:36:01 +0000

    Automatically backed up by Learn

--- db/01_create_artists.rb
+++ db/01_create_artists.rb
@@ -15,10 +15,7 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     end
   end
 
-  ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/artists.sqlite"
-  )
+
   sql = <<-SQL
   CREATE TABLE IF NOT EXISTS artists (
     id INTEGER PRIMARY KEY,
